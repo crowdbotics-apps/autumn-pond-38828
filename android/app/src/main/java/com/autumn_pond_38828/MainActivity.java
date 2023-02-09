9ab944d13ef367488bc4c5ddcb518fba225c3e32
Crowdbotics <team@crowdbotics.com>
2023-02-09 10:26:51 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/autumn_pond_38828/MainActivity.java
@@ -0,0 +1,15 @@
+package com.autumn_pond_38828;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "autumn_pond_38828";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "autumn-pond-38828",
+  url: "https://autumn-pond-38828.botics.co",
+  api: "https://autumn-pond-38828.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
